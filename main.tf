5986c6da7138c3a082b6df7fbb506cbb1e111074
srbharath <srbharath41@gmail.com>
2023-07-30 12:47:42 +0530

    backend edited

--- main.tf
+++ main.tf
@@ -122,3 +122,20 @@ terraform {
     prefix = "sftp-to-bucket/terraform.tfstate"
   }
 }
+
+resource "google_cloud_run_v2_job" "default" {
+  name     = "Cloud-Run-job-demo"
+  location = "us-central1"
+
+  template {
+    template {
+      containers {
+        image = var.Cloud_Run_image
+      }
+  vpc_access {
+    connector = google_vpc_access_connector.default.id
+      egress    = "ALL_TRAFFIC"
+    }
+    }
+  }
+}
